97cae5abd2b8d9709982c560565cbbe8862b0249
areum Yang <81684775+yangareum1818@users.noreply.github.com>
2023-06-28 17:07:38 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.15 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,18 @@
+function solution(t, p) {
+    var answer = 0;
+    let pLen = p.length;
+    for(let i = 0; i < t.length; i++) {
+        let tt = t.slice(i, pLen++);
+        if (tt.length === p.length) {
+            if (tt <= p) {
+                answer += 1;
+            }
+        }
+    }
+    return answer;
+}
+
+// 1. p의 길이를 변수에 담아 구한다. pLen
+// 2. t의 길이만큼 반복문을 돌린다.
+// 3. 새로운 변수(tt)에 담아 slice 함수를 이용해 index를 시작으로 pLen길이만큼 문자열을 잘라준다.
+// 4. 조건문을 이용해 tt의 길이와 p의 길이가 같을 경우, tt가 p보다 작거나 같을 경우 answer변수에 +1을 더해 몇개인지 담아준다.
