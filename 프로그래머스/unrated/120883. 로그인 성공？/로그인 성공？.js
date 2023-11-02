3ad862cefa4f6a74b23a7f7b8ece069ae4636e65
areum Yang <81684775+yangareum1818@users.noreply.github.com>
2023-11-03 00:39:18 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,40 @@
+function solution(id_pw, db) {
+    let answer = '';
+    const [id, pw] = id_pw;
+    
+//     두번째 풀이.
+    for(let i = 0; i < db.length; i++) {
+        const IDs = db[i][0];
+        const PWs = db[i][1];
+        
+        if (IDs === id) {
+            if (PWs === pw) {
+                answer = 'login';
+                break;
+            } else {
+                answer = 'wrong pw';
+                break;
+            }
+        } else {
+            answer = 'fail';
+        }
+        
+        // // ID PW 모두 동일한 값이 있을 경우.
+        // if (PWs === true ) {
+        //     if (IDs === true) {
+        //         return answer = 'login'l
+        //     }
+        // }
+        // // ID는 일치하지만, PW가 틀릴 경우.
+        // if (PWs === false) {
+        //     if (IDs === true) {
+        //         return answer = 'wrong pw';
+        //     }
+        // }
+        // // ID PW 모두 일치하지 않을 경우.
+        // if (PWs && IDs === false ) {
+        //     return answer = 'fail';
+        // }
+    }
+    return answer;
+}
