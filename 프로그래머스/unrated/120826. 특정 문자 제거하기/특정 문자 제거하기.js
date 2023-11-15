c22554f53d53501d449f93fba00d2c74a1c7399e
areum Yang <81684775+yangareum1818@users.noreply.github.com>
2023-11-16 02:15:39 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -9,3 +9,7 @@ function solution(my_string, letter) {
     }
     return answer = my_string.join('');
 }
+
+// 다른 사람들의 풀이. 습관성반복문 금지하고 메소드함수를 적극활용하자..
+// return my_string.split(letter).join('');
+// return my_string.replaceAll(letter, "");
