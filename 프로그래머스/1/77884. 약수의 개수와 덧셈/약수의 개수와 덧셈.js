e5ef613894996881451b89fcb52aa84c4e54aedc
areum Yang <81684775+yangareum1818@users.noreply.github.com>
2023-12-06 19:04:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -11,4 +11,5 @@ function solution(left, right) {
     return answer;
 }
 
-// 1. left부터 right까지의 약수의 갯수를 구한다.
+// 1. 이중 반복문으로 left부터 right까지의 약수의 갯수를 구한다. 변수 num.
+// 2. 이중 반복문안에서 나온 num값을 짝, 홀수를 구분해 조건을 건다. i의 값을 +할지, -할지.
