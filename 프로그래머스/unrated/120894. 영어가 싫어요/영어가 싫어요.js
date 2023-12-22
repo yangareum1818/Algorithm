2ac5b23db13214f4676449cde29ce64be2a2df01
areum Yang <81684775+yangareum1818@users.noreply.github.com>
2023-12-22 23:29:54 +0900

    [level unrated] Title: 영어가 싫어요, Time: 2.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -2,7 +2,6 @@ function solution(numbers) {
     var answer = 0;
     var result = '';
     var abc = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    numbers = numbers.split('')
     
     for(let i = 0; i < numbers.length; i++) {
         result += numbers[i];
@@ -14,3 +13,13 @@ function solution(numbers) {
     }
     return Number(answer);
 }
+
+// 1. 0-9의 영어를 배열에 담은 변수를 만든다.
+// 2. 반복문을 돌려 문자열을 담을 변수를 하나 만들어서 변수에 하나씩 담아준다.
+// 3. 조건을 거는데, 0-9까지 만든 배열안에 2번의 변수가 있다면 ?
+// 4. 0-9까지 담은 배열에서 2번에 담은 변수가 배열의 몇번째 위치에 있는지를 찾고 answer변수에 담아준다.
+// 4-1. 담았을 때, 2번의 값은 초기화가 되어 다시 빈값으로 다음값을 찾는다.
+// 4-2. answer변수는 숫자이기때문에 String으로 변환해 answer로 담아준다. ( 그렇지 않으면 담은 숫자들이 더해진다.)
+// 4-3. 마지막 return값에 answer은 String 타입이기때문에 Number로 다시 변환해준다.
+
+// 처음 answer의 초깃값을 ''인 문자형으로 해줬다면 String으로 바꿔주지 않아도된다.
