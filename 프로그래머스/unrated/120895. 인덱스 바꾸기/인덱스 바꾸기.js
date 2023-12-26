e9dd7b284f43bb489da4ebb15c40b9aa190fac1f
areum Yang <yangareum1818@nate.com>
2023-12-26 21:05:49 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,6 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]]
+    return answer = my_string.join('');
+}
